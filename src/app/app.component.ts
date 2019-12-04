137e79fbff697235cd72805042a8f80e79c5e52e
Jagerchief <dani.jimenez@waynabox.com>
2019-12-04 16:47:33 +0100

    added subscriber in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Store} from '@ngrx/store';
+import {AppState} from './actions/app.state';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-pdf';
+
+  constructor(private store: Store<AppState>) {
+    store.subscribe( function (storeContent) {
+      console.log(storeContent);
+    });
+  }
 }
